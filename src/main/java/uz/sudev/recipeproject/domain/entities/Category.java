7306b9d87a706f407ee3ec0fcbe02ae6fcbab3bf
sukhrobdev <suxrobdevbk@gmail.com>
2022-11-01 18:37:53 +0500

    Done!

--- src/main/java/uz/sudev/recipeproject/domain/entities/Category.java
+++ src/main/java/uz/sudev/recipeproject/domain/entities/Category.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import java.util.Set;
 
 @Entity
