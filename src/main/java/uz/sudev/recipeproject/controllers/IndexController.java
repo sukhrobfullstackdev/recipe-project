2e3ea1c1a1e01f5a2694c5aac669c44e66bbccca
sukhrobdev <suxrobdevbk@gmail.com>
2022-11-01 16:55:03 +0500

    Removed unused imports!

--- src/main/java/uz/sudev/recipeproject/controllers/IndexController.java
+++ src/main/java/uz/sudev/recipeproject/controllers/IndexController.java
@@ -3,23 +3,14 @@ package uz.sudev.recipeproject.controllers;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uz.sudev.recipeproject.domain.entities.Category;
-import uz.sudev.recipeproject.domain.entities.UnitOfMeasure;
-import uz.sudev.recipeproject.repositories.CategoryRepository;
-import uz.sudev.recipeproject.repositories.RecipeRepository;
-import uz.sudev.recipeproject.repositories.UnitOfMeasureRepository;
 import uz.sudev.recipeproject.services.interfaces.RecipeService;
-
-import java.util.Optional;
 
 @RestController
 public class IndexController {
     private final RecipeService recipeService;
-
     public IndexController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
-
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
         model.addAttribute("recipes",recipeService.getRecipes());
