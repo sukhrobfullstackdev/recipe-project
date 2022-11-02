4f7a0e51931660a11a12cdf9de0f1a3053d9d98e
sukhrobdev <suxrobdevbk@gmail.com>
2022-11-02 18:19:05 +0500

    Created Service , Controller and page for showing single recipe!

--- src/main/java/uz/sudev/recipeproject/services/implementations/RecipeServiceImpl.java
+++ src/main/java/uz/sudev/recipeproject/services/implementations/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import uz.sudev.recipeproject.repositories.RecipeRepository;
 import uz.sudev.recipeproject.services.interfaces.RecipeService;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 @Service
 public class RecipeServiceImpl implements RecipeService {
@@ -20,5 +21,16 @@ public class RecipeServiceImpl implements RecipeService {
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
+    }
+    @Override
+    public Recipe findById(Long l) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+
+        if (recipeOptional.isEmpty()) {
+            throw new RuntimeException("Recipe Not Found!");
+        }
+
+        return recipeOptional.get();
     }
 }
